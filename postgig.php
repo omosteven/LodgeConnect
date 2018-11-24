c20342d1f5f3da5c5d572dbe160a66991321888d
omosteven <omosteven123@gmail.com>
2018-11-24 14:30:25 +0100

    Add files via upload

---
+++ postgig.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(include('lodgeconnectDB.php'))   { //To import the Database connection file
+    if(include('hashalgorithm.php'))    { //Import the hashing script here
+        if(isset($_SESSION['loggedEmail'])) {
+            header('location:uploadfile.php');
+            }
+            else {
+                header('location:login.html');
+            }
+        }
+            else    {
+                header('Location:pageerrorhandler.php');
+            }
+    }   
+    else    {
+        header('Location:pageerrorhandler.php');
+    }
+
+?>
+            
