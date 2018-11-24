de41a4d6c60bdd8859e0958f6d6d2fdd2e907d77
omosteven <omosteven123@gmail.com>
2018-11-24 14:35:02 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<link href="css/footer.css" rel="stylesheet">
+<div class = 'border' style='margin-top:auto;'>
+<div class="navbar">
+    <div class='head1'><button class='btn btn-signin'><a class='txt' href='feedback.html'>Feedbacks</a></button> </div>
+    <div class='head1'><button class='btn btn-signin'><a class='txt' href='contact.html'>Constact Us</a></button> </div>
+    <div class='head1'><button class='btn btn-signin'><a class='txt' href='about.html'>About Us</a></button> </div>
+</div>
+</div>
